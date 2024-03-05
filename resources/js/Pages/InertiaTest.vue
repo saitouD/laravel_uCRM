6fcfd701774d62ad9504ff06df2bfd57e8d5fd6f
p-031 <p-031@p-031noMacBook-Air.local>
2024-03-05 11:21:15 +0900

    リンクコンポーネントの作成1

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {Link} from "@inertiajs/inertia-vue3";
+</script>
+
+<template>
+    Inertiaテストです。<br>
+    <a href="/">aタグでwelcomeに移動</a><br>
+    <Link href="/">LinkでWelcomeに移動</Link><br>
+    <Link :href="route('inertia.index')">名前付きルート</Link><br>
+    <Link :href="route('inertia.show', { id : 1 })">button</Link><br>
+    
+</template>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\InertiaTestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,6 +25,14 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+
+Route::get('inertia-test', function(){
+    return Inertia::render('InertiaTest');
+});
+
+Route::get('inertia/index', [InertiaTestController::class, 'index'])->name('inertia.index');
+
+Route::get('inertia/show/{id}', [InertiaTestController::class, 'show'])->name('inertia.show');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');

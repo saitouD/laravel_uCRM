fd3e29ed1fd249ffa5035b65cd08458f25f8c87c
saitouD <saitou_d@thinkone.jp>
2024-03-07 09:05:38 +0900

    create storeメソッドの作成

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
+use inertia\Inertia;
 use App\Models\Item;
 
 class ItemController extends Controller
@@ -15,7 +16,10 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Items/Index',[
+            'items' => Item::select('id','name','price','is_selling')
+            ->get()
+        ]);
     }
 
     /**
@@ -25,7 +29,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
@@ -36,7 +40,14 @@ class ItemController extends Controller
      */
     public function store(StoreItemRequest $request)
     {
-        //
+        Item::create([
+            'name' => $request->name,
+            'memo' => $request->memo,
+            'price' => $request->price,
+        ]);
+
+
+        return to_route('items.index');
     }
 
     /**
@@ -47,7 +58,7 @@ class ItemController extends Controller
      */
     public function show(Item $item)
     {
-        //
+
     }
 
     /**
--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class ItemSeeder extends Seeder
 {
@@ -14,6 +16,23 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('items')->insert([
+            [
+            'name' => 'カット',
+            'memo' => '内容',
+            'price' =>6000 ,
+            ],
+            [
+            'name' => 'カラー',
+            'memo' => '内容',
+            'price' =>8000 ,
+            ],
+            [
+            'name' => 'パーマ',
+            'memo' => '内容',
+            'price' =>10000 ,
+            ]   
+            
+        ]);
     }
 }

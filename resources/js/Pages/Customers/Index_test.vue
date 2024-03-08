87cc53b0d08de7efab4029409fe88ae76b9d42b1
saitouD <saitou_d@thinkone.jp>
2024-03-08 11:33:50 +0900

    ペジネート機能の追加

---
+++ resources/js/Pages/Customers/Index_test.vue
@@ -0,0 +1,18 @@
+å<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+import FlashMessage from '@/Components/FlashMessage.vue'
+//import { onMounted } from 'vue';  onMountedページを読み込んだタイミングで動作する
+import Pagination from '@/Components/Pagination.vue'
+
+const props = defineProps({
+    customers: Object, //paginateしてDBから受け取る場合は、形式が変わるのでarrayでなくObjectにする。
+})
+
+// onMounted(() =>{ 
+//     console.log(props.customers)
+// })
+</script>
+
+<template>
+</template>
